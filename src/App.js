b3557945acb9ff41e1371cdd43c98818c61be9cb
kparker028 <60025945+kparker028@users.noreply.github.com>
2021-06-11 17:53:09 -0600

    chore: more deletions of react app package, imported new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Carousel } from "./components/Carousel/carousel";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='title-container'>
+				<h1>React API project</h1>
+			</div>
+			<Carousel />
+		</div>
+	);
 }
 
 export default App;
